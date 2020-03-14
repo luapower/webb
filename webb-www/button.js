7702593fbcfaefdbcc86608103e474af7230c180
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2020-03-14 16:47:31 +0200

    unimportant

---
+++ webb-www/button.js
@@ -0,0 +1,24 @@
+
+function button(...options) {
+
+	let b = {}
+
+	function init() {
+		update(b, ...options)
+		create()
+	}
+
+	function create() {
+		b.button = H.button({class: 'button'}, b.title)
+		if (b.parent)
+			b.parent.add(b.button)
+		b.button.class('primary', b.primary == true)
+		b.button.on('click', b.click)
+	}
+
+	init()
+
+	return b
+
+}
+
